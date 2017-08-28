cc8644cd2c53b67d025b643ceeb3eb1873e2806b
Vinayak Mehrotra <mehrotravinayak98@gmail.com>
2017-08-28 22:03:52 +0530

    A typical C Program

---
+++ program5.c
@@ -0,0 +1,12 @@
+//Write a program to take a number from the user and print the sum of all the digits in that number
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter any number=");
+    scanf("%d",&n);
+    int i, sum=0;
+    for(i=n;i>0;i=i/10)
+        sum=sum+i%10;
+    printf("Sum of digits of %d=%d",n,sum);
+}
